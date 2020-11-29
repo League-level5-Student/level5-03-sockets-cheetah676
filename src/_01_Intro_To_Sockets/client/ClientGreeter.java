2b73b2c73d1637cdddea10ed40de9a7f1aa9357a
cheetah676 <jake.mordan@gmail.com>
2020-11-29 10:16:37 -0800

    chat app done

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -8,22 +8,28 @@ public class ClientGreeter {
    public static void main(String [] args) {
 	  //1. Create a String for the ip address of the server. 
 	  // If you don't know how to find a computer's ip address, ask about ifconfig on linux/mac and ipconfig on windows.
-      //String ip=ipconfig;
+      String ip="192.168.1.75";
       //2. Create an integer for the server's port number
-      
+      int portNum=8080;
       //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
-    
+  
     	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
- 
+ try {
+	Socket socket=new Socket(ip, portNum);
          //5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
-         
+         DataOutputStream dos=new DataOutputStream(socket.getOutputStream());
          //6. Use the DataOutputStream object to send a message to the server using the writeUTF(String message) method
-         
+         dos.writeUTF("Hi server");
          //7. Create a DataInputStream object. When initializing it, use the Server object you created in step 4 to call the getInputStream() method.
-         
+         DataInputStream dis=new DataInputStream(socket.getInputStream());
          //8. Use the DataInputStream object to print a message from the server using the readUTF() method.
-         
+         System.out.println(dis.readUTF());
          //9. Close the client's server object
-
+         socket.close();
+   }
+ catch (IOException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
    }
 }
--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,9 +1,61 @@
 package _02_Chat_Application;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+import _00_Click_Chat.networking.Client;
+import _00_Click_Chat.networking.Server;
+
 /*
  * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
  */
 
-public class ChatApp {
-
+public class ChatApp extends JFrame{
+static JTextField text=new JTextField();
+static String message="";
+	ChatServer server;
+	ChatClient client;
+	
+	
+	public static void main(String[] args) {
+		new ChatApp();
+	}
+	
+	public ChatApp(){
+		
+		int response = JOptionPane.showConfirmDialog(null, "Would you like to host a connection?", "Chat!", JOptionPane.YES_NO_OPTION);
+		if(response == JOptionPane.YES_OPTION){
+			server = new ChatServer(8080);
+			setTitle("SERVER");
+			JOptionPane.showMessageDialog(null, "Server started at: " + server.getIPAddress() + "\nPort: " + server.getPort());
+			text.addActionListener((e)->{
+				message=text.getText();
+				server.sendClick();
+			});
+			add(text);
+			setVisible(true);
+			setSize(400, 300);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			server.start();
+			
+		}else{
+			setTitle("CLIENT");
+			String ipStr = JOptionPane.showInputDialog("Enter the IP Address");
+			String prtStr = JOptionPane.showInputDialog("Enter the port number");
+			int port = Integer.parseInt(prtStr);
+			client = new ChatClient(ipStr, port);
+			text.addActionListener((e)->{
+				message=text.getText();
+				client.sendClick();
+			});
+			add(text);
+			setVisible(true);
+			setSize(400, 300);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			message=text.getText();
+			client.start();
+		}
+		
+	}
 }
